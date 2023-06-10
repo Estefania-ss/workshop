01604de3ffb587fe2a34b6aae989e4aeb8d1eaf1
webtech <webtech@webtech-debian.webtech>
2023-06-10 15:58:03 -0400

    crear index de tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_10_175447) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_10_193620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_175447) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_Tweets_on_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "body", default: "", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_10_175447) do
   end
 
   add_foreign_key "Tweets", "users"
+  add_foreign_key "tweets", "users"
 end
